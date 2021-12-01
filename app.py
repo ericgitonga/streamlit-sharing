0f94ada62154fc84d25972c9cd9d07a968a4a219
Eric Gitonga <gitonga@gmail.com>
2021-12-01 14:40:07 +0300

    change to photo editor code

--- app.py
+++ app.py
@@ -1,19 +1,41 @@
 import streamlit as st
+import numpy as np
+from PIL import Image
 
-st.title("Temperature Controller")
+st.title("Super Basic Photo Editor")
 
-step = 0.5
+def display_figures(img1, img2):
+    fig1_left, fig2_right = st.columns(2)
+    with fig1_left:
+        st.markdown("### Original image")
+        st.image(img1)
+    with fig2_right:
+        st.markdown("### Processed image")
+        st.image(img2)
 
-if "temperature" not in st.session_state:
-    st.session_state["temperature"] = 25.0
+image_file = st.sidebar.file_uploader("Upload an image file", type=["jpg","png","tif"])
 
-raise_temperature = st.button("Raise temperature")
-if raise_temperature:
-    st.session_state["temperature"] += step
-lower_temperature = st.button("Lower temperature")
-if lower_temperature:
-    st.session_state["temperature"] -= step
-st.write("The temperature will be set to ",
-         st.session_state["temperature"],
-         "degrees.")
+if image_file is not None:
+    input_image = Image.open(image_file)
 
+    effect = st.sidebar.selectbox("Select the type of image manipulation.",
+         ["Rotate Image", "Flip Image", "Resize"])
+        
+    if effect == "Rotate Image":
+        angle = st.sidebar.select_slider("Select the angle of rotation", [0, 90, 180, 270, 360])
+        output_image = input_image.rotate(angle)
+
+    if effect == "Flip Image":
+        mirror_choice = st.sidebar.radio("Choose  how to flip the image",
+                             ["Horizontally", "Vertically"])
+        if mirror_choice == "Horizontally":
+            output_image = input_image.transpose(Image.FLIP_LEFT_RIGHT)
+        else:
+            output_image = input_image.transpose(Image.FLIP_TOP_BOTTOM)
+
+    if effect == "Resize":
+        scale_factor = st.sidebar.slider("Select the percentage size", 5, 10, 6)
+        output_image = input_image.resize((input_image.width//scale_factor, input_image.height//scale_factor))
+
+    if st.button("Process image"):
+	    display_figures(input_image, output_image)
